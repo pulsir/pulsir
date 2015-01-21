8d6f1deff2504ed9f3418f5a7e50219e4c0a50e7
Mario Borna Mjertan <bor.mbm@gmail.com>
2015-01-21 19:29:09 +0100

    super beautiful integration with Android 5 & iOS
    it's so beautiful :) I think only Chrome v39+ supports this on Android, but take a look at it:
    
    http://imgur.com/xWuNM3j / http://imgur.com/QozCNwY

--- template/whitey/header.php
+++ template/whitey/header.php
@@ -1,4 +1,4 @@
-                                                                <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
@@ -7,10 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{BASEURL}}template/whitey/css/generate.php">
     <link rel="shortcut icon" href="http://d.pulsir.eu/assets/midres-final2-logo.png">
+    <link rel="icon" href="http://d.pulsir.eu/assets/midres-final2-logo.png">
+    <link rel="apple-touch-icon" href="http://d.pulsir.eu/assets/midres-final2-logo.png">
     <meta name="google-site-verification" content="6ERT0DsecI_-Bqg5KmKxgRgsexypl_umxuqVcRn2fxU" />
+    <meta name="theme-color" content="#247f77">
     <link href="{{BASEURL}}template/whitey/font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet">
     {{IF;pagetitle;Search for something;template/whitey/search_header_tweaks.php;}}
   </head>
   <body>
 
-                            
+                      
