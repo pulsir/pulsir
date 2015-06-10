84c71b3774c74c1ee4161e800b0df605e1ac6ddb
Mario Borna Mjertan <bor.mbm@gmail.com>
2015-06-10 20:59:10 +0200

    Update footer.php

--- template/whitey/footer.php
+++ template/whitey/footer.php
@@ -1,7 +1,7 @@
  </div>
 </div>
 <div class="footer">
-  <p>This site is made by the <a href="http://team.pulsir.co">Pulsir team</a>. &middot; <a href="http://github.com/pulsir/pulsir">Source</a> &middot; Pulsir is released under <a href="http://license.visualidiot.com">the :) license</a></p>
+  <p>This site is made by the <a href="http://team.pulsir.co">Pulsir team</a> and <a href="http://educateam.hr/heartfilled">Heartfilled</a>. &middot; <a href="http://github.com/pulsir/pulsir">Source</a> &middot; Pulsir is released under <a href="http://license.visualidiot.com">the :) license</a></p>
   <p>Friends:
     <a href="http://internetdefenseleague.org">The Internet Defense League</a>
     &middot;
@@ -9,6 +9,7 @@
   </p>
 </div>
 </div>
+
 <script src="//oss.maxcdn.com/libs/jquery/2.0.3/jquery.min.js"></script>
 <script src="{{BASEURL}}template/whitey/whitey.js"></script>
 <script>
